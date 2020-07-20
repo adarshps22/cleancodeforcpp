c5802095524e60a4fc53d53e2c3b3266c0d63f22
adarshps22 <adarshps22@gmail.com>
2020-07-20 15:39:08 +0530

    Add functions

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,6 +1,22 @@
+bool is_bpm_ok(float bpm)
+{
+  if(bpm < 70 || bpm > 150) {
+    return false;
+  }
+  return true;
+}
+
+bool is_resp_rate_ok(float respRate)
+{
+   if(respRate < 30 || respRate > 60) {
+    return false;
+  }
+  
+  return true;
+}
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150 || spo2 < 80 || respRate < 30 || respRate > 60) {
+  if(!is_bpm_ok(bpm) || spo2 < 80 || !is_resp_rate_ok(respRate)) {
     return false;
   }
   return true;
