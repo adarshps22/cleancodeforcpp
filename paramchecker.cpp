28a40335368a6098b9691c5e6f81c27e1e5cd03e
adarshps22 <adarshps22@gmail.com>
2020-07-20 15:29:31 +0530

    check vitals

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,10 +1,6 @@
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
+  if(bpm < 70 || bpm > 150 || spo2 < 80 || respRate < 30 || respRate > 60) {
     return false;
   }
   return true;
