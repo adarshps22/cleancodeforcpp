c99ffc88925197405bf07252f69e514f84da9975
adarshps22 <adarshps22@gmail.com>
2020-07-22 10:44:56 +0530

    abstraction for check vitals

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,18 +1,90 @@
-bool is_bpm_ok(float bpm)
-{
-  return bpm < 70 || bpm > 150;
-}
+#include <list>
 
-bool is_resp_rate_ok(float respRate)
-{
-   return respRate < 30 || respRate > 60;
-}
+static class range_validator {
+public:
+    static bool in_range_exclusive_limit(float llimit, float ulimit, float value)
+    {
+        return value > llimit && value < ulimit;
+    }
 
-bool is_spo2_ok(float spo2)
+    static bool less_than_limit(float llimit, float val)
+    {
+        return val < llimit;
+    }
+};
+
+class vital {
+public:
+    virtual bool is_vital_ok() = 0;
+};
+
+class bpm_vital : public vital {
+    float _ulimit = 150;
+    float _llimit = 70;
+    float _bpm;
+
+public:
+    bpm_vital(float val) : _bpm(val) {}
+
+    bool  is_vital_ok()
+    {
+        return range_validator::in_range_exclusive_limit(_llimit, _ulimit, _bpm);
+    }
+};
+
+class spo2 : public vital
 {
-  return  spo2 < 80;
-}
+    float _llimit = 80;
+    float _spo2;
+
+public:
+    spo2_vital(float val) : _spo2(val) {}
+
+    bool  is_vital_ok()
+    {
+        return !range_validator::less_than_limit(_llimit, _spo2);
+    }
+};
+
+class resp_rate_vital : public vital
+{
+    float _llimit = 30;
+    float _ulimit = 60;
+    float _resp_rate;
+
+public:
+    resp_rate_vital(float val) : _resp_rate(val){}
+
+    bool  is_vital_ok()
+    {
+        return range_validator::in_range_exclusive_limit(_llimit, _ulimit, _resp_rate);
+    }
+};
+
+class check_vitals
+{
+public:
+    bool all_vitals_ok(std::list<vital*> vitals)
+    {
+        for (auto v : vitals)
+        {
+            if (!v->is_vital_ok())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (is_bpm_ok(bpm) || is_resp_rate_ok(spo2) || is_spo2_ok(respRate));
+  
+    std::list<vital*> vitals;
+    vitals.push_back(&bpm_vital(bpm));
+    vitals.push_back(&spo2_vital(spo2));
+    vitals.push_back(&resp_rate_vital(respRate));
+
+    check_vitals vital_condition;
+
+    return vital_condition.all_vitals_ok(vitals);
 }
